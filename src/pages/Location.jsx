195a4ac1a3d849055abc8a3381f5ec2be0a7ced2
yeajin458 <yeajin040508@naver.com>
2026-07-25 01:41:05 +0900

    네비게이션 스타일 수정, 로그인 상태 추가

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -3,16 +3,21 @@ import { useNavigate } from "react-router-dom";
 import { getLocations } from "../api/locationApi";
 import { checkLocation } from "../api/locationApi";
 import logo from "../assets/logo.png";
+import rouletteIcon from "../assets/roulette.png";
+import locationIcon from "../assets/location.png";
+import loginIcon from "../assets/login.png";
+import logoutIcon from "../assets/logout.png";
 import "./../css/location.css";
 
 function Location() {
   const [locations, setLocations] = useState([]);
   const [selectedId, setSelectedId] = useState(null);
   const navigate = useNavigate();
-
+  const [isLogin, setIsLogin] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
+    checkLogin();
     loadLocation();
   }, []);
 
@@ -20,6 +25,13 @@ function Location() {
     const response = await getLocations();
 
     setLocations(response.data.locations);
+  };
+  const checkLogin = async () => {
+    const response = await axios.get("http://localhost:3000/regist/check", {
+      withCredentials: true,
+    });
+
+    setIsLogin(response.data.isLogin);
   };
 
   const handleCheck = async (id) => {
@@ -48,9 +60,25 @@ function Location() {
 
         {menuOpen && (
           <div className="menu">
-            <p onClick={() => navigate("/roulette")}>룰렛 돌리기</p>
-            <p onClick={() => navigate("/location/view")}>내 주소</p>
-            <p onClick={() => navigate("/regist/logout")}>로그아웃</p>
+            <p onClick={() => navigate("/roulette")}>
+              <img src={rouletteIcon} alt="룰렛" />
+              룰렛 돌리기
+            </p>
+            <p onClick={() => navigate("/location/view")}>
+              {" "}
+              <img src={locationIcon} alt="주소" />내 주소
+            </p>
+            {isLogin ? (
+              <p onClick={() => navigate("/regist/logout")}>
+                <img src={logoutIcon} alt="로그아웃" />
+                로그아웃
+              </p>
+            ) : (
+              <p onClick={() => navigate("/regist/login")}>
+                <img src={loginIcon} alt="로그인" />
+                로그인
+              </p>
+            )}
           </div>
         )}
       </div>
