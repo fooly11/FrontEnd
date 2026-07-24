9b99fbb9864c1f82c39a0de3a8fe92d554c81a97
yeajin458 <yeajin040508@naver.com>
2026-07-25 01:42:55 +0900

    네비게이션 스타일 수정, 로그인 상태 추가

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { getLocations } from "../api/locationApi";
 import { checkLocation } from "../api/locationApi";
